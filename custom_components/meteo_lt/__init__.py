0520bd72b06b24de969442207f9b7370aa5259dd
braticks <85012808+braticks@users.noreply.github.com>
2024-09-30 17:09:34 +0300

    Update __init__.py

--- custom_components/meteo_lt/__init__.py
+++ custom_components/meteo_lt/__init__.py
@@ -1,27 +1,29 @@
-from homeassistant.core import HomeAssistant
+"""The Meteo.lt integration."""
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
 from .const import DOMAIN
 
 PLATFORMS = ["sensor", "weather"]
 
-async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+async def async_setup(hass: HomeAssistant, config: dict):
     """Set up the Meteo.lt component."""
     hass.data.setdefault(DOMAIN, {})
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up Meteo.lt from a config entry."""
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = entry.data
-    
+
     for platform in PLATFORMS:
         hass.async_create_task(
             hass.config_entries.async_forward_entry_setup(entry, platform)
         )
-    
+
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Unload a config entry."""
     unload_ok = all(
         await asyncio.gather(
