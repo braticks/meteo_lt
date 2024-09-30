d044bb5cde6b98139ab86450f839c881f3953348
braticks <85012808+braticks@users.noreply.github.com>
2024-09-30 16:39:41 +0300

    Update config_flow.py

--- custom_components/meteo_lt/config_flow.py
+++ custom_components/meteo_lt/config_flow.py
@@ -10,12 +10,16 @@ class MeteoLtConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
 
     async def async_step_user(self, user_input=None) -> FlowResult:
+        errors = {}
+
         if user_input is not None:
+            # You can add validation here if needed
             return self.async_create_entry(title=user_input[CONF_LOCATION], data=user_input)
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
                 vol.Required(CONF_LOCATION, default=DEFAULT_LOCATION): str,
-            })
+            }),
+            errors=errors,
         )
