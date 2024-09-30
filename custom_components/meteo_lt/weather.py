f6bfaab702495af8cf1a7e7862c63eed5cea99f6
braticks <85012808+braticks@users.noreply.github.com>
2024-09-30 17:10:49 +0300

    Update weather.py

--- custom_components/meteo_lt/weather.py
+++ custom_components/meteo_lt/weather.py
@@ -1,3 +1,4 @@
+"""Weather platform for Meteo.lt integration."""
 from homeassistant.components.weather import WeatherEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
@@ -5,38 +6,11 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback) -> None:
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
+    """Set up Meteo.lt weather platform."""
     location = entry.data["location"]
     async_add_entities([MeteoLtWeather(location)])
 
 class MeteoLtWeather(WeatherEntity):
-    def __init__(self, location):
-        self._location = location
-        self._attr_unique_id = f"meteo_lt_weather_{location}"
-        self._attr_name = f"Meteo.lt Weather {location.capitalize()}"
-        self._attr_temperature = None
-        self._attr_humidity = None
-        self._attr_wind_speed = None
-        self._attr_condition = None
-
-    @property
-    def temperature(self):
-        return self._attr_temperature
-
-    @property
-    def humidity(self):
-        return self._attr_humidity
-
-    @property
-    def wind_speed(self):
-        return self._attr_wind_speed
-
-    @property
-    def condition(self):
-        return self._attr_condition
-
-    async def async_update(self):
-        # Implement update logic here
-        # This is where you'll fetch data from the Meteo.lt API
-        # and update self._attr_temperature, self._attr_humidity, etc.
-        pass
+    """Representation of a Meteo.lt weather entity."""
+    # Implement weather entity logic here
