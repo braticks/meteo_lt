10e0e0a308b0920b84cd56b25318e425bda5f505
braticks <85012808+braticks@users.noreply.github.com>
2024-09-30 17:09:18 +0300

    Update config_flow.py

--- custom_components/meteo_lt/config_flow.py
+++ custom_components/meteo_lt/config_flow.py
@@ -1,33 +1,51 @@
-from typing import Any, Dict, Optional
+"""Config flow for Meteo.lt integration."""
 import voluptuous as vol
+import requests
 
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
-from homeassistant.data_entry_flow import FlowResult
-import homeassistant.helpers.config_validation as cv
+from homeassistant.core import callback
+from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
-from .const import DOMAIN, CONF_LOCATION, DEFAULT_LOCATION
+from .const import DOMAIN, CONF_LOCATION, CONNECTION_ERROR, LOCATION_ERROR
+
+def get_available_locations():
+    """Get available locations from Meteo.lt API."""
+    try:
+        response = requests.get("https://api.meteo.lt/v1/places")
+        if response.status_code == 200:
+            places = response.json()
+            return {place['code']: place['name'] for place in places}
+        else:
+            return None
+    except requests.RequestException:
+        return None
 
 class MeteoLtConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Meteo.lt."""
 
     VERSION = 1
 
-    async def async_step_user(
-        self, user_input: Optional[Dict[str, Any]] = None
-    ) -> FlowResult:
+    async def async_step_user(self, user_input=None):
         """Handle the initial step."""
-        errors: Dict[str, str] = {}
+        errors = {}
+        locations = await self.hass.async_add_executor_job(get_available_locations)
+
+        if locations is None:
+            errors["base"] = CONNECTION_ERROR
+
         if user_input is not None:
-            return self.async_create_entry(
-                title=user_input[CONF_LOCATION],
-                data=user_input,
-            )
+            if user_input[CONF_LOCATION] in locations:
+                return self.async_create_entry(
+                    title=locations[user_input[CONF_LOCATION]],
+                    data=user_input
+                )
+            else:
+                errors[CONF_LOCATION] = LOCATION_ERROR
 
         return self.async_show_form(
             step_id="user",
-            data_schema=vol.Schema(
-                {vol.Required(CONF_LOCATION, default=DEFAULT_LOCATION): str}
-            ),
+            data_schema=vol.Schema({
+                vol.Required(CONF_LOCATION): vol.In(locations or {})
+            }),
             errors=errors,
         )
