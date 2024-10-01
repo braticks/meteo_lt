920ca90e4fec0896fb748bcfbbac485ea18a3cdd
braticks <85012808+braticks@users.noreply.github.com>
2024-10-01 08:58:53 +0300

    Update config_flow.py

--- custom_components/meteo_lt/config_flow.py
+++ custom_components/meteo_lt/config_flow.py
@@ -14,6 +14,8 @@ class MeteoLtConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle the initial step."""
         errors = {}
         if user_input is not None:
+            await self.async_set_unique_id(user_input[CONF_LOCATION])
+            self._abort_if_unique_id_configured()
             return self.async_create_entry(title=user_input[CONF_LOCATION], data=user_input)
 
         data_schema = vol.Schema({
@@ -25,26 +27,23 @@ class MeteoLtConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     @staticmethod
     @callback
     def async_get_options_flow(config_entry):
-        return OptionsFlowHandler(config_entry)
+        return MeteoLtOptionsFlow(config_entry)
 
-class OptionsFlowHandler(config_entries.OptionsFlow):
-    """Handle options flow."""
+class MeteoLtOptionsFlow(config_entries.OptionsFlow):
+    """Handle options flow for Meteo.lt."""
 
     def __init__(self, config_entry):
+        """Initialize options flow."""
         self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
         """Manage the options."""
-        return await self.async_step_user()
-
-    async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
-        errors = {}
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
-        data_schema = vol.Schema({
-            vol.Required(CONF_LOCATION, default=self.config_entry.data.get(CONF_LOCATION)): str
-        })
-
-        return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema({
+                vol.Required(CONF_LOCATION, default=self.config_entry.data.get(CONF_LOCATION)): str
+            })
+        )
