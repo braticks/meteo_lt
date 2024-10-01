76290fd65e86be5d21356c707e37608c0e87c0bc
braticks <85012808+braticks@users.noreply.github.com>
2024-10-01 08:44:24 +0300

    Update const.py

--- custom_components/meteo_lt/const.py
+++ custom_components/meteo_lt/const.py
@@ -1,10 +1,4 @@
 """Constants for the Meteo.lt integration."""
 
-from homeassistant.helpers.update_coordinator import UpdateFailed
-
 DOMAIN = "meteo_lt"
 CONF_LOCATION = "location"
-DEFAULT_LOCATION = "vilnius"
-
-LOCATION_ERROR = "invalid_location"
-CONNECTION_ERROR = "cannot_connect"
