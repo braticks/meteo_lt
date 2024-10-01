371e39f2690119593af31796f43ea12e134a4f92
braticks <85012808+braticks@users.noreply.github.com>
2024-10-01 08:44:37 +0300

    Update config_flow.py

--- custom_components/meteo_lt/config_flow.py
+++ custom_components/meteo_lt/config_flow.py
@@ -1,24 +1,9 @@
 """Config flow for Meteo.lt integration."""
-import voluptuous as vol
-import requests
-
 from homeassistant import config_entries
 from homeassistant.core import callback
-from homeassistant.helpers.aiohttp_client import async_get_clientsession
+import voluptuous as vol
 
-from .const import DOMAIN, CONF_LOCATION, CONNECTION_ERROR, LOCATION_ERROR
-
-def get_available_locations():
-    """Get available locations from Meteo.lt API."""
-    try:
-        response = requests.get("https://api.meteo.lt/v1/places")
-        if response.status_code == 200:
-            places = response.json()
-            return {place['code']: place['name'] for place in places}
-        else:
-            return None
-    except requests.RequestException:
-        return None
+from .const import DOMAIN, CONF_LOCATION
 
 class MeteoLtConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Meteo.lt."""
@@ -28,24 +13,38 @@ class MeteoLtConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         errors = {}
-        locations = await self.hass.async_add_executor_job(get_available_locations)
-
-        if locations is None:
-            errors["base"] = CONNECTION_ERROR
-
         if user_input is not None:
-            if user_input[CONF_LOCATION] in locations:
-                return self.async_create_entry(
-                    title=locations[user_input[CONF_LOCATION]],
-                    data=user_input
-                )
-            else:
-                errors[CONF_LOCATION] = LOCATION_ERROR
+            return self.async_create_entry(title=user_input[CONF_LOCATION], data=user_input)
 
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({
-                vol.Required(CONF_LOCATION): vol.In(locations or {})
-            }),
-            errors=errors,
-        )
+        data_schema = vol.Schema({
+            vol.Required(CONF_LOCATION): str
+        })
+
+        return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return OptionsFlowHandler(config_entry)
+
+class OptionsFlowHandler(config_entries.OptionsFlow):
+    """Handle options flow."""
+
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        """Manage the options."""
+        return await self.async_step_user()
+
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        errors = {}
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        data_schema = vol.Schema({
+            vol.Required(CONF_LOCATION, default=self.config_entry.data.get(CONF_LOCATION)): str
+        })
+
+        return self.async_show_form(step_id="user", data_schema=data_schema, errors=errors)
